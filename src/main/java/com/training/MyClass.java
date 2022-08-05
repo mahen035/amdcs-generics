7216b2399fe3f679432c1ec5c4664da906395a81
mkumar <test@mail.com>
2022-08-05 11:27:58 +0530

    initial commit

---
+++ src/main/java/com/training/MyClass.java
@@ -0,0 +1,14 @@
+package com.training;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
+
+}
+
+//Git: Local Repository
+
+//GitHub: Remote Repository
